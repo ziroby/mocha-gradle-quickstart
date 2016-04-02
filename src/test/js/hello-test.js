202bb09c5038fd851f9978a105778e0a624dd3d2
Ron Ziroby Romero <ziroby@ziroby.com>
2016-04-02 09:32:16 -0500

    Added mocha unit test framework

---
+++ src/test/js/hello-test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+
+describe('Hello Module', function () {
+  it('Should have true = true', function () {
+    expect(true).to.equal(true)
+  });
+})
